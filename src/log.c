83d39e22e048181246bfc8903385a25fc831fed1
Geoff Greer <geoff@cloudkick.com>
2011-11-27 15:05:16 -0800

    More logging helper stuff

--- src/log.c
+++ src/log.c
@@ -9,13 +9,45 @@ void set_log_level(enum log_level threshold) {
     log_threshold = threshold;
 }
 
-void plog(const int level, const char *fmt, ...) {
+void log_debug(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vplog(LOG_LEVEL_DEBUG, fmt, args);
+    va_end(args);
+}
+
+void log_msg(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vplog(LOG_LEVEL_MSG, fmt, args);
+    va_end(args);
+}
+
+void log_warn(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vplog(LOG_LEVEL_WARN, fmt, args);
+    va_end(args);
+}
+
+void log_err(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vplog(LOG_LEVEL_ERR, fmt, args);
+    va_end(args);
+}
+
+void vplog(const int level, const char *fmt, va_list args) {
     if (level < log_threshold) {
         return;
     }
 
+    vprintf(fmt, args);
+}
+
+void plog(const int level, const char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
-    vprintf(fmt, args);
+    vplog(level, fmt, args);
     va_end(args);
 }
--- src/log.h
+++ src/log.h
@@ -8,6 +8,14 @@ enum log_level {
     LOG_LEVEL_ERR   = 40
 };
 
+void set_log_level(enum log_level threshold);
+
+void log_debug(const char *fmt, ...);
+void log_msg(const char *fmt, ...);
+void log_warn(const char *fmt, ...);
+void log_err(const char *fmt, ...);
+
+void vplog(const int level, const char *fmt, va_list args);
 void plog(const int level, const char *fmt, ...);
 
 #endif
