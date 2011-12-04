d10b3be7c1f3772b63cd7c37e064791d4ef3a244
Geoff Greer <geoff@cloudkick.com>
2011-12-04 13:55:13 -0800

    More options

--- src/options.c
+++ src/options.c
@@ -4,19 +4,6 @@
 
 #include "options.h"
 
-static struct option longopts[] = {
-    { "after", required_argument, NULL, 'A' },
-    { "before", required_argument, NULL, 'B' },
-    { "context", required_argument, NULL, 'C' },
-    { "ackmate", no_argument, NULL, 0 },
-    { "follow", no_argument, NULL, 'f' },
-    { "nofollow", no_argument, NULL, 0 },
-    { "ignore-case", no_argument, NULL, 0 },
-    { "nosmart-case", no_argument, NULL, 0 },
-    { "ackmate-dir-filter", no_argument, NULL, 0 },
-    { NULL, 0, NULL, 0 }
-};
-
 cli_options *parse_options(int argc, char **argv) {
     int ch;
 
@@ -26,8 +13,23 @@ cli_options *parse_options(int argc, char **argv) {
     opts->recurse_dirs = 1;
     opts->casing = CASE_SENSITIVE_RETRY_INSENSITIVE;
 
+    struct option longopts[] = {
+        { "after", required_argument, NULL, 'A' },
+        { "before", required_argument, NULL, 'B' },
+        { "context", required_argument, NULL, 'C' },
+        { "ackmate", no_argument, NULL, 0 },
+        { "follow", no_argument, &(opts->follow_symlinks), 1 },
+        { "nofollow", no_argument, &(opts->follow_symlinks), 0 },
+        { "ignore-case", no_argument, NULL, 'i' },
+        { "nosmart-case", no_argument, NULL, 0 },
+        { "ackmate-dir-filter", required_argument, NULL, 0 },
+        { NULL, 0, NULL, 0 }
+    };
+
+    int opt_index = 0;
+
     // XXX: this is nowhere near done
-    while ((ch = getopt_long(argc, argv, "A:B:C:f", longopts, NULL)) != -1) {
+    while ((ch = getopt_long(argc, argv, "A:B:C:fi", longopts, &opt_index)) != -1) {
         switch (ch) {
             case 'A':
                 opts->after = atoi(optarg);
@@ -41,6 +43,14 @@ cli_options *parse_options(int argc, char **argv) {
             case 'f':
                 opts->follow_symlinks = 1;
                 break;
+            case 'i':
+                opts->casing = CASE_INSENSITIVE;
+                break;
+            case 0: // Long option
+                // Continue to usage if we don't recognize the option
+                if (longopts[opt_index].flag != 0) {
+                    break;
+                }
             default:
                 // print usage here or something
                 printf("blah\n");
