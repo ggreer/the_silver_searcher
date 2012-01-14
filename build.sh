4668634e32da322f8218370e283a0bc08bf9c873
Geoff Greer <geoff@greer.fm>
2012-01-14 11:53:20 -0800

    Durrr I want this to be a directory, not executable.

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ AC_SEARCH_OPTS=""
 
 # This is to make life easier for people who installed pkg-config in /usr/local
 # but have autoconf/make/etc in /usr/. AKA most mac users
-if [ -x "/usr/local/share/aclocal" ]
+if [ -d "/usr/local/share/aclocal" ]
 then
     AC_SEARCH_OPTS="-I /usr/local/share/aclocal"
 fi
