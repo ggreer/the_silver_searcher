54adfce0cf7099f7a13819ef5032347542c5938a
Geoff Greer <geoff@greer.fm>
2012-01-14 11:49:48 -0800

    Make building easier for a lot of mac users AKA me.

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-aclocal && \
+AC_SEARCH_OPTS=""
+
+# This is to make life easier for people who installed pkg-config in /usr/local
+# but have autoconf/make/etc in /usr/. AKA most mac users
+if [ -x "/usr/local/share/aclocal" ]
+then
+    AC_SEARCH_OPTS="-I /usr/local/share/aclocal"
+fi
+
+aclocal $AC_SEARCH_OPTS && \
 autoconf && \
 autoheader && \
 automake --add-missing && \
