f3398f648c5bf22458b87b5c5a7688979765015b
Geoff Greer <geoff@greer.fm>
2012-03-02 15:25:31 -0800

    Remove unneeded includes from search.h

--- src/search.h
+++ src/search.h
@@ -3,27 +3,20 @@
 
 #include <dirent.h>
 #include <errno.h>
-#include <limits.h>
 #include <fcntl.h>
 #include <pcre.h>
-#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/dir.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <unistd.h>
 
 #include "ignore.h"
 #include "log.h"
 #include "options.h"
 #include "print.h"
 #include "util.h"
-
-#include "config.h"
 
 ag_stats stats;
 
