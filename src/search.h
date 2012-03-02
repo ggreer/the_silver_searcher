12bbfe2ef33968410dc7c199625ce84df76de82e
Geoff Greer <geoff@greer.fm>
2012-03-02 15:20:58 -0800

    Move search stuff into search.c and search.h. Stub out searching stdin.

---
+++ src/search.h
@@ -0,0 +1,36 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include <dirent.h>
+#include <errno.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <pcre.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/dir.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "ignore.h"
+#include "log.h"
+#include "options.h"
+#include "print.h"
+#include "util.h"
+
+#include "config.h"
+
+ag_stats stats;
+
+size_t skip_lookup[256];
+
+int search_buf();
+int search_stdin(const pcre *re, const pcre_extra *re_extra);
+int search_dir(const pcre *re, const pcre_extra *re_extra, const char* path, const int depth);
+
+#endif
