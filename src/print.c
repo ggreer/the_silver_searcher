3b61caca996538e24e051484f5c4c729c319f5b5
Geoff Greer <geoff@greer.fm>
2012-03-03 06:34:38 +0000

    Fix a double-printing bug.

--- src/print.c
+++ src/print.c
@@ -76,53 +76,55 @@ void print_file_matches(const char* path, const char* buf, const int buf_len, co
                 printf("--\n");
             }
 
-            if (lines_since_last_match > 0 && opts.before > 0) {
-                /* TODO: this is buggy as hell */
-                /* TODO: yeah, it totally is */
-                /* print the previous line(s) */
-                int lines_to_print = lines_since_last_match - (opts.before + opts.after + 1);
-                if (lines_to_print < 0) {
-                    lines_to_print = opts.before;
-                }
-                if (lines_to_print > opts.before) {
-                    lines_to_print = 0;
-                }
+            if (lines_since_last_match > 0) {
+                if (opts.before > 0) {
+                    /* TODO: this is buggy as hell */
+                    /* TODO: yeah, it totally is */
+                    /* print the previous line(s) */
+                    int lines_to_print = lines_since_last_match - (opts.before + opts.after + 1);
+                    if (lines_to_print < 0) {
+                        lines_to_print = opts.before;
+                    }
+                    if (lines_to_print > opts.before) {
+                        lines_to_print = 0;
+                    }
 
-                for (j = lines_to_print; j < opts.before; j++) {
-                    prev_line = (last_prev_line + j) % opts.before;
-                    if (context_prev_lines[prev_line] != NULL) {
-                        if (opts.print_heading == 0) {
-                            print_path(path);
-                            printf(":");
-                        }
-                        if (opts.color) {
-                            printf("%s%i%s%c%s\n", colors_line_number, line - (opts.before - j), colors_reset, sep, context_prev_lines[prev_line]);
-                        }
-                        else {
-                            printf("%i%c%s\n", line - (opts.before - j), sep, context_prev_lines[prev_line]);
+                    for (j = lines_to_print; j < opts.before; j++) {
+                        prev_line = (last_prev_line + j) % opts.before;
+                        if (context_prev_lines[prev_line] != NULL) {
+                            if (opts.print_heading == 0) {
+                                print_path(path);
+                                printf(":");
+                            }
+                            if (opts.color) {
+                                printf("%s%i%s%c%s\n", colors_line_number, line - (opts.before - j), colors_reset, sep, context_prev_lines[prev_line]);
+                            }
+                            else {
+                                printf("%i%c%s\n", line - (opts.before - j), sep, context_prev_lines[prev_line]);
+                            }
                         }
                     }
                 }
-            }
 
-            if (opts.ackmate == 0) {
-                if (opts.print_heading == 0) {
-                    print_path(path);
-                    printf(":");
-                }
-                if (opts.color) {
-                    printf("%s%i%s:", colors_line_number, line, colors_reset);
-                }
-                else {
-                    printf("%i:", line);
-                }
+                if (opts.ackmate == 0) {
+                    if (opts.print_heading == 0) {
+                        print_path(path);
+                        printf(":");
+                    }
+                    if (opts.color) {
+                        printf("%s%i%s:", colors_line_number, line, colors_reset);
+                    }
+                    else {
+                        printf("%i:", line);
+                    }
 
-                if (opts.column) {
-                    printf("%i:", column + 1);
-                }
-                /* print up to current char */
-                for (j = prev_line_offset; j < i; j++) {
-                    putchar(buf[j]);
+                    if (opts.column) {
+                        printf("%i:", column + 1);
+                    }
+                    /* print up to current char */
+                    for (j = prev_line_offset; j < i; j++) {
+                        putchar(buf[j]);
+                    }
                 }
             }
 
