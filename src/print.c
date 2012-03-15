8922d47fb623a555e4cce2d58934a0b5f5a4a30d
Geoff Greer <geoff@greer.fm>
2012-03-16 02:04:07 +0900

    This comment was stale. Whoops.

--- src/print.c
+++ src/print.c
@@ -115,8 +115,7 @@ void print_file_matches(const char* path, const char* buf, const int buf_len, co
             if (context_prev_lines[last_prev_line] != NULL) {
                 free(context_prev_lines[last_prev_line]);
             }
-            /* We just incremented column so it will always be at least 1.
-             * We don't want to strcpy the \n */
+            /* We don't want to strcpy the \n */
             context_prev_lines[last_prev_line] = strndup(&buf[prev_line_offset], i - prev_line_offset);
             last_prev_line = (last_prev_line + 1) % opts.before;
         }
