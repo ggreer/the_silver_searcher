5e99d25ec226480800baeb455779734a1eaa5c2f
Geoff Greer <geoff@greer.fm>
2012-08-21 13:34:05 -0700

    Functions are back under control

--- src/util.h
+++ src/util.h
@@ -32,7 +32,6 @@ typedef char *(*strncmp_fp)(const char*, const char*, const size_t, const size_t
 
 void generate_skip_lookup(const char *find, size_t f_len, size_t skip_lookup[], int case_sensitive);
 
-/* OK, these functions are getting a little out of control */
 char* boyer_moore_strnstr(const char *s, const char *find, const size_t s_len, const size_t f_len, const size_t skip_lookup[]);
 char* boyer_moore_strncasestr(const char *s, const char *find, const size_t s_len, const size_t f_len, const size_t skip_lookup[]);
 
