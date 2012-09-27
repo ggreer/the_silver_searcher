9e53ba90cd80a600d944fb652fff4fea62c13a37
Geoff Greer <geoff@greer.fm>
2012-09-27 14:29:54 -0700

    Remove strlcat

--- src/util.c
+++ src/util.c
@@ -223,38 +223,6 @@ int contains_uppercase(const char* s) {
     return FALSE;
 }
 
-#ifndef HAVE_STRLCAT
-/*
- * strlcat and strlcpy, taken from Apache Traffic Server
- */
-size_t strlcat(char *dst, const char *src, size_t siz)
-{
-    char *d = dst;
-    const char *s = src;
-    size_t n = siz;
-    size_t dlen;
-
-  /* Find the end of dst and adjust bytes left but don't go past end */
-    while (n-- != 0 && *d != '\0')
-      d++;
-    dlen = d - dst;
-    n = siz - dlen;
-
-    if (n == 0)
-      return (dlen + strlen(s));
-    while (*s != '\0') {
-      if (n != 1) {
-        *d++ = *s;
-        n--;
-      }
-      s++;
-    }
-    *d = '\0';
-
-    return (dlen + (s - src));  /* count does not include NUL */
-}
-#endif
-
 #ifndef HAVE_STRLCPY
 size_t strlcpy(char *dst, const char *src, size_t siz)
 {
--- src/util.h
+++ src/util.h
@@ -54,9 +54,6 @@ int is_whitespace(const char ch);
 
 int contains_uppercase(const char* s);
 
-#ifndef HAVE_STRLCAT
-size_t strlcat(char *dest, const char *src, size_t size);
-#endif
 #ifndef HAVE_STRLCPY
 size_t strlcpy(char *dest, const char *src, size_t size);
 #endif
