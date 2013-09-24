f9a2563fc8275c23d84d8e3c305bfa8cfdac679f
monochromegane <dev.kuro.obi@gmail.com>
2013-09-24 23:20:34 +0900

    Fixed non-initial bytes of multibyte code.

--- src/util.c
+++ src/util.c
@@ -187,12 +187,12 @@ int is_binary(const void* buf, const int buf_len) {
             /* UTF-8 detection */
             if (buf_c[i] > 193 && buf_c[i] < 224 && i + 1 < total_bytes) {
                 i++;
-                if (buf_c[i] < 192) {
+                if (buf_c[i] > 127 && buf_c[i] < 192) {
                     continue;
                 }
             } else if (buf_c[i] > 223 && buf_c[i] < 240 && i + 2 < total_bytes) {
                 i++;
-                if (buf_c[i] < 192 && buf_c[i + 1] < 192) {
+                if (buf_c[i] > 127 && buf_c[i] < 192 && buf_c[i + 1] > 127 && buf_c[i + 1] < 192) {
                     i++;
                     continue;
                 }
