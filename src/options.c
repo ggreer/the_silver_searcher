8cd9dd942d8ebe5c0b0b4593a9d64ae42fb07684
byplayer <byplayer100@gmail.com>
2014-03-12 16:40:36 +0800

    fix condition

--- src/options.c
+++ src/options.c
@@ -236,7 +236,7 @@ void parse_options(int argc, char **argv, char **base_paths[], char **paths[]) {
     }
 
     rv = fstat(fileno(stdin), &statbuf);
-    if (rv != 0) {
+    if (rv == 0) {
         if (S_ISFIFO(statbuf.st_mode)) {
             opts.search_stream = 1;
         }
