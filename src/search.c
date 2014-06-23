84a517f79943ffc1a8b7dfab5f715449ca4dc3ee
gao hongwei <hhww.gao@gmail.com>
2014-06-23 16:13:04 +0800

    Let earch in a file following the option --[no]heading. As if searching in the editor(eg. vim), with Ag --nogroup <cword> % the editor can lotate the search result with the file name and line number

--- src/search.c
+++ src/search.c
@@ -411,10 +411,6 @@ void search_dir(ignores *ig, const char *base_path, const char *path, const int 
     } else if (results == -1) {
         if (errno == ENOTDIR) {
             /* Not a directory. Probably a file. */
-            /* If we're only searching one file, don't print the filename header at the top. */
-            if (depth == 0 && opts.paths_len == 1) {
-                opts.print_heading = -1;
-            }
             search_file(path);
         } else {
             log_err("Error opening directory %s: %s", path, strerror(errno));
