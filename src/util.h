35245ac12b7ba59243b2b62db99996f66d99a9bf
Geoff Greer <geoff@greer.fm>
2014-07-27 21:06:33 -0700

    Const-ify some things. Use find_skip_lookup in boyer_moore_strncasestr.

--- src/util.h
+++ src/util.h
@@ -52,10 +52,11 @@ ag_stats stats;
 
 typedef const char *(*strncmp_fp)(const char *, const char *, const size_t, const size_t, const size_t[], const size_t *);
 
-void generate_alpha_skip(const char *find, size_t f_len, size_t skip_lookup[], int case_sensitive);
-int is_prefix(const char *s, const size_t s_len, const size_t pos);
-size_t suffix_len(const char *s, const size_t s_len, const size_t pos);
-void generate_find_skip(const char *find, size_t f_len, size_t **skip_lookup, int case_sensitive);
+void generate_alpha_skip(const char *find, size_t f_len, size_t skip_lookup[], const int case_sensitive);
+int is_prefix(const char *s, const size_t s_len, const size_t pos, const int case_sensitive);
+size_t suffix_len(const char *s, const size_t s_len, const size_t pos, const int case_sensitive);
+void generate_find_skip(const char *find, const size_t f_len, size_t **skip_lookup, const int case_sensitive);
+
 size_t max(size_t a, size_t b);
 
 const char *boyer_moore_strnstr(const char *s, const char *find, const size_t s_len, const size_t f_len,
