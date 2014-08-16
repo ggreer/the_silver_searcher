f341e44998ea7f425db26782686363ac51c73a76
Michael Reed <supertron421@gmail.com>
2014-08-15 22:17:40 -0400

    whitespace cleanup

--- src/decompress.c
+++ src/decompress.c
@@ -22,7 +22,7 @@ const uint8_t LZMA_HEADER_SOMETIMES[3] = { 0x5D, 0x00, 0x00 };
  *
  * zpipe.c: example of proper use of zlib's inflate() and deflate()
  *    Not copyrighted -- provided to the public domain
- *    Version 1.4  11 December 2005  Mark Adler 
+ *    Version 1.4  11 December 2005  Mark Adler
  */
 static void *decompress_zlib(const void *buf, const int buf_len,
                              const char *dir_full_path, int *new_buf_len) {
@@ -221,7 +221,7 @@ ag_compression_type is_zipped(const void *buf, const int buf_len) {
     /* Zip magic numbers
      * compressed file: { 0x1F, 0x9B }
      * http://en.wikipedia.org/wiki/Compress
-     * 
+     *
      * gzip file:       { 0x1F, 0x8B }
      * http://www.gzip.org/zlib/rfc-gzip.html#file-format
      *
