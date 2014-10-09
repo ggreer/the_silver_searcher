8e43ee95317ffcd8302d7e4270103812cc42e376
Geoff Greer <geoff@greer.fm>
2014-10-08 20:55:59 -0700

    Obey some more matches that we should be obeying.

--- src/ignore.c
+++ src/ignore.c
@@ -280,7 +280,7 @@ static int path_ignore_search(const ignores *ig, const char *path, const char *f
         match_pos = binary_search(slash_filename, ig->names, 0, ig->names_len);
         if (match_pos >= 0) {
             log_debug("file %s ignored because name matches static pattern %s", temp, ig->names[match_pos]);
-            free(temp );
+            free(temp);
             return 1;
         }
 
@@ -290,6 +290,20 @@ static int path_ignore_search(const ignores *ig, const char *path, const char *f
             free(temp);
             return 1;
         }
+
+        for (i = 0; i < ig->names_len; i++) {
+            char *pos = strstr(slash_filename, ig->names[i]);
+            if (pos == slash_filename || (pos && *(pos - 1) == '/')) {
+                pos += strlen(ig->names[i]);
+                if (*pos == '\0' || *pos == '/') {
+                    log_debug("file %s ignored because path somewhere matches name %s", slash_filename, ig->names[i]);
+                    free(temp);
+                    return 1;
+                }
+            }
+            log_debug("pattern %s doesn't match name %s", slash_filename, ig->names[i]);
+        }
+
         for (i = 0; i < ig->slash_regexes_len; i++) {
             if (fnmatch(ig->slash_regexes[i], slash_filename, fnmatch_flags) == 0) {
                 log_debug("file %s ignored because name matches slash regex pattern %s", slash_filename, ig->slash_regexes[i]);
