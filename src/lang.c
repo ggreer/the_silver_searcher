8cf5fc6368d6878f694b05b1549137d4dd17beb0
abdul dakkak <abduld@wolfram.com>
2015-03-01 23:49:11 -0600

    Add Mathematica language filetype option

--- src/lang.c
+++ src/lang.c
@@ -42,6 +42,7 @@ lang_spec_t langs[] = {
     { "markdown", { "markdown", "mdown", "mdwn", "mkdn", "mkd", "md" } },
     { "mason", { "mas", "mhtml", "mpl", "mtxt" } },
     { "matlab", { "m" } },
+    { "mathematica", { "m", "wl" } },
     { "mercury", { "m", "moo" } },
     { "objc", { "m", "h" } },
     { "objcpp", { "mm", "h" } },
