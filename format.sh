32c5903682891f22e9a67e08f206717d84e3a3e2
Geoff Greer <geoff@greer.fm>
2015-03-29 02:09:11 -0700

    Formatting script now works with different versions of clang-format and can test as well as reformat.

--- format.sh
+++ format.sh
@@ -1,5 +1,43 @@
 #!/bin/bash
 
-CLANG_FORMAT=clang-format-3.5
+function usage() {
+    echo "Usage: $0 test|reformat"
+}
 
-$CLANG_FORMAT -i src/*.c
+if [ $# -eq 0 ]
+then
+    usage
+    exit 0
+fi
+
+if [ -z "$CLANG_FORMAT" ]
+then
+    CLANG_FORMAT=clang-format-3.5
+    echo "No CLANG_FORMAT set. Using $CLANG_FORMAT"
+fi
+
+SOURCE_FILES=`git ls-files src/`
+
+if [ "$1" == "reformat" ]
+then
+    echo "Reformatting source files"
+    echo $CLANG_FORMAT -style=file -i $SOURCE_FILES
+    $CLANG_FORMAT -style=file -i $SOURCE_FILES
+    exit 0
+elif  [ "$1" == "test" ]
+then
+    RESULT=`$CLANG_FORMAT -style=file -output-replacements-xml $SOURCE_FILES | grep -c '<replacement '`
+    if [ $RESULT -eq 0 ]
+    then
+        echo "code is formatted correctly :)"
+        exit 0
+    else
+        echo "code is not formatted correctly! :("
+        echo "Run '$0 reformat' to fix formatting"
+        exit 1
+    fi
+else
+    echo "invalid command: $1"
+    usage
+    exit 1
+fi
