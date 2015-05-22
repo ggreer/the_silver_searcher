3b8e6090c2164cee5517c368be78bdbc341e87b3
Avi Halachmi (:avih) <avihpit@yahoo.com>
2015-05-22 13:00:39 +0300

    (local) msys2 build.sh

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@ cd `dirname $0`
 
 AC_SEARCH_OPTS=""
 # For those of us with pkg-config and other tools in /usr/local
-PATH=$PATH:/usr/local/bin
+PATH=$PATH:/usr/bin
 
 # This is to make life easier for people who installed pkg-config in /usr/local
 # but have autoconf/make/etc in /usr/. AKA most mac users
-if [ -d "/usr/local/share/aclocal" ]
+if [ -d "/usr/share/aclocal" ]
 then
-    AC_SEARCH_OPTS="-I /usr/local/share/aclocal"
+    AC_SEARCH_OPTS="-I /usr/share/aclocal"
 fi
 
 aclocal $AC_SEARCH_OPTS && \
