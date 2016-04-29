4f215652de53099a3ad472a62ffa5ad6de70a283
Shlomi Fish <shlomif@shlomifish.org>
2016-04-29 10:31:39 +0300

    Remove a no longer relevant comment.
    About invert match and print_only_filenames. Per the request at
    https://github.com/ggreer/the_silver_searcher/pull/888#issuecomment-215617833
    .

--- src/search.c
+++ src/search.c
@@ -184,13 +184,6 @@ multiline_done:
         }
         pthread_mutex_lock(&print_mtx);
         if (opts.print_filename_only) {
-            /* If the --files-without-matches or -L option is passed we should
-             * not print a matching line. This option currently sets
-             * opts.print_filename_only and opts.invert_match. Unfortunately
-             * setting the latter has the side effect of making matches.len = 1
-             * on a file-without-matches which is not desired behaviour. See
-             * GitHub issue 206 for the consequences if this behaviour is not
-             * checked. */
             if (!opts.invert_match) {
                 if (opts.print_count) {
                     print_path_count(dir_full_path, opts.path_sep, (size_t)matches_len);
