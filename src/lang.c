be6d07f51c8fc2f718810191bed57d683f1abbf8
Jeff Federman <jefffederman@gmail.com>
2017-02-01 13:34:21 -0500

    Add .es6 to js file type

--- src/lang.c
+++ src/lang.c
@@ -40,7 +40,7 @@ lang_spec_t langs[] = {
     { "ini", { "ini" } },
     { "jade", { "jade" } },
     { "java", { "java", "properties" } },
-    { "js", { "js", "jsx", "vue" } },
+    { "js", { "es6", "js", "jsx", "vue" } },
     { "json", { "json" } },
     { "jsp", { "jsp", "jspx", "jhtm", "jhtml" } },
     { "julia", { "jl" } },
