c8dbb0a0eaeab1e0405ffe02ddd28b99dc031b0d
JFLarvoire <jf.larvoire@free.fr>
2017-03-05 15:29:47 +0100

    Leave the log file in the build directory itself.

--- configure.ag.bat
+++ configure.ag.bat
@@ -12,6 +12,8 @@
 :#                                                                            *
 :#*****************************************************************************
 
-:# To avoid overwriting the existing WIN32 subdirectories in msc_libs\zlib and ag\src,
-:# output all build files in the bin subdirectory.
-if not defined OUTDIR set "OUTDIR=bin"
+:# To avoid overwriting the existing WIN32 subdirectories in msc_libs\zlib and ag\src...
+if not defined OUTDIR ( :# Unless already redirected elsewhere
+  set "OUTDIR=bin"	&rem :# Output all built files in the bin subdirectory. (Instead of the default WIN32 and WIN64)
+  set "LOGDIR=."	&rem :# But leave the log file in the build directory itself.
+)
