d61e081a968d5d6f92eabe5c3f55ae0f642b9a96
JFLarvoire <jf.larvoire@free.fr>
2017-03-12 19:31:31 +0100

    Avoid trying to build ag.exe for DOS and WIN95, which are not supported yet.

--- configure.ag.bat
+++ configure.ag.bat
@@ -17,3 +17,7 @@ if not defined OUTDIR ( :# Unless already redirected elsewhere
   set "OUTDIR=bin"	&rem :# Output all built files in the bin subdirectory. (Instead of the default WIN32 and WIN64)
   set "LOGDIR=."	&rem :# But leave the log file in the build directory itself.
 )
+
+:# Avoid trying to build ag.exe for DOS and WIN95, which are not supported yet.
+if "%OS%"=="Windows_NT" set "OS=" &:# This is the Windows' homonym default, same as undefined for us here.
+if not defined OS set "OS=WIN32 WIN64" &:# Only WIN32 and WIN64 are supported so far.
